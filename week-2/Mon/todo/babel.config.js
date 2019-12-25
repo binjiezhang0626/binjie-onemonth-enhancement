90b5bb9b4d528f35f8fdf8c6af48b5309ae3667a
Binjie <im.binjie.zhang@outlook.com>
2019-12-25 10:18:27 +0800

    start learn babel and eject one react-app

--- week-2/Mon/todo/babel.config.js
+++ week-2/Mon/todo/babel.config.js
@@ -1,16 +1,19 @@
-const presets = [
-  [
-    "@babel/env",
-    {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1",
-      },
-      useBuiltIns: "usage",
-    },
-  ],
-];
+module.exports = function (api) {
+  api.cache(true);
 
-module.exports = { presets };
+  const presets = [
+    [
+      "@babel/env",
+      {
+        targets: "> 0.25%, not dead",
+        useBuiltIns: "usage",
+      },
+    ],
+  ];
+  // const plugins = [ ... ];
+
+  return {
+    presets,
+    // plugins
+  };
+}
